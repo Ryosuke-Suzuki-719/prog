30e1bcfa608ac19c739d9c8b2d5868072d511a1e
Ryosuke-Suzuki-719 <78679844+Ryosuke-Suzuki-719@users.noreply.github.com>
2021-05-01 02:24:03 +0900

    Add files via upload

---
+++ kadai2/customer_register_act.php
@@ -0,0 +1,58 @@
+<?php
+
+// var_dump($_POST);
+// exit();
+
+// 関数ファイル読み込み
+include('functions.php');
+
+// データ受け取り
+$name = $_POST["name"];
+$ezoca = $_POST["ezoca"];
+
+// DB接続関数
+$pdo = connect_to_db();
+
+// ユーザ存在有無確認
+$sql = 'SELECT COUNT(*) FROM customer_table WHERE ezoca=:ezoca';
+
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':ezoca', $ezoca, PDO::PARAM_STR);
+$status = $stmt->execute();
+
+if ($status == false) {
+  // SQL実行に失敗した場合はここでエラーを出力し，以降の処理を中止する
+  $error = $stmt->errorInfo();
+  echo json_encode(["error_msg" => "{$error[2]}"]);
+  exit();
+}
+
+if ($stmt->fetchColumn() > 0) {
+  // usernameが1件以上該当した場合はエラーを表示して元のページに戻る
+  // $count = $stmt->fetchColumn();
+  echo "<p>すでに登録されているお客様です。</p>";
+  echo '<a href="customer_search_index.php">お客様検索画面に戻る</a>';
+  exit();
+}
+
+// ユーザ登録SQL作成
+// `created_at`と`updated_at`には実行時の`sysdate()`関数を用いて実行時の日時を入力する
+$sql = 'INSERT INTO customer_table(id, name, ezoca, is_admin, is_deleted, created_at, updated_at) VALUES(NULL, :name, :ezoca, 0, 0, sysdate(), sysdate())';
+
+// SQL準備&実行
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+$stmt->bindValue(':ezoca', $ezoca, PDO::PARAM_STR);
+$status = $stmt->execute();
+
+// データ登録処理後
+if ($status == false) {
+  // SQL実行に失敗した場合はここでエラーを出力し，以降の処理を中止する
+  $error = $stmt->errorInfo();
+  echo json_encode(["error_msg" => "{$error[2]}"]);
+  exit();
+} else {
+  // 正常にSQLが実行された場合は入力ページファイルに移動し，入力ページの処理を実行する
+  header("Location:customer_search_index.php");
+  exit();
+}
